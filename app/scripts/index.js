ce7b8727ab0c01f2a998ee1f4bce90d07879d086
Mikhail Litvinov <litvinovmiknik@gmail.com>
2021-01-25 19:40:24 +0500

    script optimization

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -9,12 +9,13 @@ $(function() {
         let area = $(this).data('area');
         $(this).parents('.row').find('.apartment-desc__area').text(area);
 
+        let imgName = room + 'k' + '-' + area + '.jpg';
+
         let src = $(this).parents('.row').find('.apartment-plan__img').attr('src');
         let arr = src.split('/');
-        arr.pop();
-        let imgName = room + 'k' + '-' + area + '.jpg';
-        arr.push(imgName);
+        arr.splice(3, 1, imgName);
         let newSrc = arr.join('/');
+        
         $(this).parents('.row').find('.apartment-plan__img').attr('src', newSrc);
     });
 });
